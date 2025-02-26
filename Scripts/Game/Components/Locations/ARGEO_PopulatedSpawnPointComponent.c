bb28951d6fd4a8f31d337adc94a01a4626b580ed
Mathieu Baudier <mbaudier@argeo.org>
2025-02-26 20:50:27 +0100

    Working default population waypoints

--- Scripts/Game/Components/Locations/ARGEO_PopulatedSpawnPointComponent.c
+++ Scripts/Game/Components/Locations/ARGEO_PopulatedSpawnPointComponent.c
@@ -5,12 +5,17 @@ class ARGEO_PopulatedSpawnPointComponentClass : SCR_AmbientPatrolSpawnPointCompo
 class ARGEO_PopulatedSpawnPointComponent : SCR_AmbientPatrolSpawnPointComponent
 {
 	protected float m_fDefaultAILimitThreshold = 0.95;
+	
+	protected AIWaypoint m_ToHomeWP;
+	protected AIWaypointCycle m_DailyLifeWP;
+
+	protected SCR_TimedWaypoint m_StayAtHomeWP;
 
 	override void EOnInit(IEntity owner)
 	{
 		super.EOnInit(owner);
 		m_fDefaultAILimitThreshold = m_fAILimitThreshold;
-		DisableSpawn();
+		DisableSpawn();// disable spawn by default
 		//Print("Populated spawnpoint initialized");
 	}
 
@@ -18,6 +23,43 @@ class ARGEO_PopulatedSpawnPointComponent : SCR_AmbientPatrolSpawnPointComponent
 	{
 		super.OnPostInit(owner);
 		//Print("Populated spawnpoint post-init");
+	}
+
+	override void PrepareWaypoints()
+	{
+		super.PrepareWaypoints();
+		
+		// !! we need at least two waypoints in prefabs hierarchy so that a cycle waypoint is created
+		AIWaypointCycle wp = AIWaypointCycle.Cast(m_Waypoint);
+		if(wp)
+		{	
+			array<AIWaypoint> waypoints = {};
+			wp.GetWaypoints(waypoints);
+			
+			// TODO make it more robust
+			m_ToHomeWP = waypoints[0];
+			m_DailyLifeWP = AIWaypointCycle.Cast(waypoints[1]);
+			
+			// checks
+			SCR_AmbientPatrolSpawnPointComponentClass componentData = SCR_AmbientPatrolSpawnPointComponentClass.Cast(GetComponentData(GetOwner()));
+			if (!componentData)
+				return;			
+			Resource waypointResource = Resource.Load(componentData.GetDefaultWaypointPrefab());			
+			if (!waypointResource || !waypointResource.IsValid())
+				return;
+			EntitySpawnParams params = EntitySpawnParams();
+			params.TransformMode = ETransformMode.WORLD;
+			params.Transform[3] = GetOwner().GetOrigin();
+			
+			m_StayAtHomeWP = SCR_TimedWaypoint.Cast(GetGame().SpawnEntityPrefab(waypointResource, null, params));
+			
+			float meanTimeAtHome = 380;
+			float timeAtHome =  Math.RandomGaussFloat(Math.Sqrt(meanTimeAtHome), meanTimeAtHome);
+			m_StayAtHomeWP.SetHoldingTime(timeAtHome);
+			// stay at home is before daily life
+			waypoints.InsertAt(m_StayAtHomeWP, 1);
+			wp.SetWaypoints(waypoints);
+		}
 	}
 
 	override void SpawnPatrol()
@@ -38,6 +80,7 @@ class ARGEO_PopulatedSpawnPointComponent : SCR_AmbientPatrolSpawnPointComponent
 	override void ActivateGroup()
 	{
 		super.ActivateGroup();
+				
 		Print("Populated spawn point - " + GetOwner().GetID() + " - activated " + m_SavedFaction.GetFactionKey());
 	}
 	
